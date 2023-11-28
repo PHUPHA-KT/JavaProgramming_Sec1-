0f06fcc7548120cb6871e5a3c8fbb2d6a435a0a2
PHUPHA-KT <phuphakongtin@gmail.com>
2023-11-28 14:12:53 +0700

    Lab4-11/28/2023

--- JavaProject_Lab4/src/PensionContributionCalculator.java
+++ JavaProject_Lab4/src/PensionContributionCalculator.java
@@ -33,26 +33,29 @@ public class PensionContributionCalculator {
 		 if (age <= 55) 
 		 { // 55 and below
 			 
-			 System.out.println("The employee's contribution is: "+frm.format(employeeContribution=salary*EMPLOYEE_RATE_55_AND_BELOW));
-			 System.out.println("The employer's contribution is: "+frm.format(employerContribution=salary*EMPLOYER_RATE_55_AND_BELOW));
-			 System.out.println("The total contribution is:"+frm.format(totalContribution=employeeContribution+employerContribution));
+			 employeeContribution=salary*EMPLOYEE_RATE_55_AND_BELOW;
+			 employerContribution=salary*EMPLOYER_RATE_55_AND_BELOW;
+		
 			 
 		 } else if (age <= 60)
 		 { // (60, 65]
-			 System.out.println("The employee's contribution is: "+frm.format(employeeContribution=salary*EMPLOYEE_RATE_55_TO_60));
-			 System.out.println("The employer's contribution is: "+frm.format(employerContribution=salary*EMPLOYER_RATE_55_TO_60));
-			 System.out.println("The total contribution is:"+frm.format(totalContribution=employeeContribution+employerContribution));
+			 employeeContribution=salary*EMPLOYEE_RATE_55_TO_60;
+			 employerContribution=salary*EMPLOYER_RATE_55_TO_60;
+		
 		 } else if (age <= 65) 
 		 { // (55, 60]
-			 System.out.println("The employee's contribution is: "+frm.format(employeeContribution=salary*EMPLOYEE_RATE_60_TO_65));
-			 System.out.println("The employer's contribution is: "+frm.format(employerContribution=salary*EMPLOYER_RATE_60_TO_65));
-			 System.out.println("The total contribution is:"+frm.format(totalContribution=employeeContribution+employerContribution));
+			 employeeContribution=salary*EMPLOYEE_RATE_60_TO_65;
+			 employerContribution=salary*EMPLOYER_RATE_60_TO_65;
+			 
 		 } else { // above 65
-			 System.out.println("The employee's contribution is: "+frm.format(employeeContribution=salary*EMPLOYEE_RATE_65_ABOVE));
-			 System.out.println("The employer's contribution is: "+frm.format(employerContribution=salary*EMPLOYER_RATE_65_ABOVE));
-			 System.out.println("The total contribution is: "+frm.format(totalContribution=employeeContribution+employerContribution));
+			 employeeContribution=salary*EMPLOYEE_RATE_65_ABOVE;
+			 employerContribution=salary*EMPLOYER_RATE_65_ABOVE;
+			 
 		 }
-		 	 
+		 totalContribution=employeeContribution+employerContribution;
+		 System.out.println("The employee's contribution is: "+frm.format(employeeContribution));
+		 System.out.println("The employer's contribution is: "+frm.format(employerContribution));
+		 System.out.println("The total contribution is: "+frm.format(totalContribution));
 	}
 
 }
